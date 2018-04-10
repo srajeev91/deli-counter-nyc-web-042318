2920d7beef1d6efd930003888fefc038e5e60fcb
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 21:18:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,8 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-  
+
+
+def now_serving(katz_deli)  
   
   
