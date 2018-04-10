4a070b812b292e71c5e56c5263561050f52631f9
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 21:02:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    result = "The line is currently: "
+    result = "The line is currently:"
     
     counter = 1
     array.each do |name|
-      result += "#{counter}. #{name} "
+      result += " #{counter}. #{name}"
       counter += 1
     end
   end
